6fe245be7c031812f37daae1320ff05fb5b1446c
Brayan Henao <bhenao@enlace.org>
2024-08-18 21:41:23 -0500

    add images gallery

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,24 +6,16 @@ import { RoundedButton } from "./components/button/RoundedButton";
 import { Services } from "./components/plans/Plans";
 import { ImageList, ImageListItem } from "@mui/material";
 
-const itemData = [
-  {
-    img: 'https://images.unsplash.com/photo-1549388604-817d15aa0110',
-    title: 'Bed',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1525097487452-6278ff080c31',
-    title: 'Books',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1523413651479-597eb2da0ad6',
-    title: 'Sink',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1563298723-dcfebaa392e3',
-    title: 'Kitchen',
-  },
+const images = [
+  { img: '/gallery/gallery1.jpg', title: 'Imagen 1' },
+  { img: '/gallery/gallery2.jpg', title: 'Imagen 2' },
+  { img: '/gallery/gallery3.jpg', title: 'Imagen 3' },
+  { img: '/gallery/gallery4.jpg', title: 'Imagen 4' },
+  { img: '/gallery/gallery5.jpg', title: 'Imagen 5' },
+  { img: '/gallery/gallery6.jpg', title: 'Imagen 6' },
+  { img: '/gallery/gallery7.jpg', title: 'Imagen 7' }
 ];
+
 
 export default function Home() {
   return (
@@ -137,14 +129,13 @@ export default function Home() {
 
           <Services />
           <section
-            className="flex p-4 flex-col w-full md:flex-row items-center gap-10 justify-center"
+            className="container mx-auto flex p-4 flex-col w-full md:flex-row items-center gap-10 justify-center"
           >
             <ImageList variant="masonry" cols={3} gap={8}>
-              {itemData.map((item) => (
+              {images.map((item, index) => (
                 <ImageListItem key={item.img}>
                   <img
-                    srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                    src={`${item.img}?w=248&fit=crop&auto=format`}
+                    src={item.img}
                     alt={item.title}
                     loading="lazy"
                   />
